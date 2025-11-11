80d212704e8b5fe86168dbd8cbe858a2737d2d2b
Kervon Ryan <kervonryan96@gmail.com>
2025-11-10 23:23:28 -0400

    Add layoutIfNeeded before transition block execution

--- SkeletonViewCore/Sources/Internal/UIKitExtensions/UIView+Transitions.swift
+++ SkeletonViewCore/Sources/Internal/UIKitExtensions/UIView+Transitions.swift
@@ -5,6 +5,9 @@ import UIKit
 extension UIView {
     
     func startTransition(transitionBlock: @escaping () -> Void) {
+        
+        layoutIfNeeded()
+        
         guard let transitionStyle = _currentSkeletonConfig?.transition,
               transitionStyle != .none else {
             transitionBlock()
